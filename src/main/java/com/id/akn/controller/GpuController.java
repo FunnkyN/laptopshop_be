fbef484aa3f0cf37622a34f42081194bbc3f91ba
deptrailoitaiai <tienanh0563@gmail.com>
2026-01-05 10:20:33 +0700

    sửa lỗi đăng nhập

--- src/main/java/com/id/akn/controller/GpuController.java
+++ src/main/java/com/id/akn/controller/GpuController.java
@@ -22,13 +22,6 @@ public class GpuController {
         return ResponseEntity.ok(gpuService.getAllGpus());
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<GpuDTO> getGpuById(@PathVariable Short id) {
-        try {
-            return ResponseEntity.ok(gpuService.getGpuDTOById(id));
-        } catch (GpuException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        }
     }
 
     @PostMapping("/api/admin")
@@ -49,6 +42,16 @@ public class GpuController {
         }
     }
 
+    
+
+    @PostMapping("/api/admin")
+    public ResponseEntity<GpuDTO> createGpu(@RequestBody GpuDTO gpuDTO) {
+        try {
+            return ResponseEntity.status(HttpStatus.CREATED).body(gpuService.createGpu(gpuDTO));
+        } catch (GpuException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+    }
     @DeleteMapping("/api/admin/{id}")
     public ResponseEntity<Void> deleteGpu(@PathVariable Short id) {
         try {
